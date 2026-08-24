203d07f7495d87011a9f9330c212115c032898c5
Edgar Camberos <157107597+EdgarCamberos1894@users.noreply.github.com>
2026-08-24 15:38:43 -0700

    refactor: remove redundant role repository

--- src/main/java/com/cambers/auth/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cambers.auth.repository;
-
-import com.cambers.auth.entity.Role;
-import com.cambers.auth.enums.RoleName;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, RoleName> {
-}
