b3b26d85c541a1a9c6e267a7aba35527828ee537
Edgar Camberos <157107597+EdgarCamberos1894@users.noreply.github.com>
2026-08-25 01:07:05 -0700

    refactor: establish account module

---
+++ src/main/java/com/cambers/auth/account/package-info.java
@@ -0,0 +1,6 @@
+@org.springframework.modulith.ApplicationModule(
+        id = "account",
+        displayName = "Account and Credentials",
+        allowedDependencies = {"delivery", "observability"}
+)
+package com.cambers.auth.account;
